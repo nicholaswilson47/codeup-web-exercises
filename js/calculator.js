7fcf7aa7962196fc5481dd7507d4a54dce0ed76b
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2020-12-30 20:17:05 -0600

    tried a dumb solution for calculator, still doesn't work

--- js/calculator.js
+++ js/calculator.js
@@ -1,42 +1,41 @@
-class Calculator{
+class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
 
-    clear(){
+    clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
 
-    delete(){
+    delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
-    appendNumber(number){
+    appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
-    chooseOperation(operation){
+    chooseOperation(operation) {
         if (this.currentOperand === '') return
-        if (this.previousOperand !== ''){
+        if (this.previousOperand !== '') {
             this.compute()
         }
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
-
     }
 
-    compute(){
+    compute() {
         let computation
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
-        if(isNaN(prev) || isNaN(current)) return
-        switch (this.operation){
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
             case '+':
                 computation = prev + current
                 break
@@ -46,7 +45,7 @@ class Calculator{
             case '*':
                 computation = prev * current
                 break
-            case '/':
+            case '÷':
                 computation = prev / current
                 break
             default:
@@ -57,35 +56,34 @@ class Calculator{
         this.previousOperand = ''
     }
 
-    getDisplayNumber(number){
+    getDisplayNumber(number) {
         const stringNumber = number.toString()
         const integerDigits = parseFloat(stringNumber.split('.')[0])
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
-        if (isNaN(integerDigits)){
+        if (isNaN(integerDigits)) {
             integerDisplay = ''
-        } else{
-            integerDisplay = integerDigits.toLocaleString('en',{maximumFractionDigits:0})
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
         }
-        if (decimalDigits != null){
-            return '$(integerDisplay).$(decimalDigits)'
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
         } else {
             return integerDisplay
         }
     }
 
-    updateDisplay(){
+    updateDisplay() {
         this.currentOperandTextElement.innerText =
             this.getDisplayNumber(this.currentOperand)
-        if (this.operation != null){
+        if (this.operation != null) {
             this.previousOperandTextElement.innerText =
-                '$(this.getDisplayNumber(this.previousOperand)) $(this.operation)'
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         } else {
             this.previousOperandTextElement.innerText = ''
         }
     }
 }
-
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -95,6 +93,7 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
