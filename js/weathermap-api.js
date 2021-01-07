2f72146975b6e6744b92293599730efb4b2475ca
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-07 10:30:45 -0600

    first steps weathermap

---
+++ js/weathermap-api.js
@@ -0,0 +1,10 @@
+
+//TO DO: CALL WEATHER APP
+$.get("http://api.openweathermap.org/data/2.5/forecast", {
+    APPID: OPEN_WEATHER_APPID,
+    lat:    29.423017,
+    lon:   -98.48527,
+    units: "imperial"
+}).done(function(data) {
+    console.log('5 day forecast', data);
+});
