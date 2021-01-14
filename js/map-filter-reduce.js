46d30229e302d0b1c9f45152c448880888a72646
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-14 09:12:12 -0600

    map-filter-reduce excercise just 1 error

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,3 +35,19 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+
+let minThreeLangs = users.filter(user => user.languages.length >= 3);
+console.log(minThreeLangs);
+
+let userEmails = users.map(user => user.email);
+console.log(userEmails);
+
+let yearsExp = user.reduce((total , user ) => total +user.yearsOfExperience, 0);
+console.log(yearsExp + " total experience, average " + (yearsExp/users.length) + " years experience per user");
+
+let longestEmail = users.reduce((currentLongest, user) => user.email.length > currentLongest.email.length ? user : currentLongest);
+console.log(longestEmailUser.name + "has the longest email address, with " + longestEmailUser.email);
+
+
+let userNames = user.reduce((listOfNames, user) => , "Your instructors are: ")
